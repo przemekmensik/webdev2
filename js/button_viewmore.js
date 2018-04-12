6bb5092ecb445143390785c152993f9f4b623ea8
Przemek Mensik <przemek.mensik@gmail.com>
2018-04-12 16:51:42 +0200

    js and blog section revised

--- js/button_viewmore.js
+++ js/button_viewmore.js
@@ -3,15 +3,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.getElementById("gallery-button").addEventListener("click", function ()  {
      var div = document.getElementById("gallery-hide");
-      if (div.style.display !== "none") {
+      if (div.style.display == "block") {
           div.style.display = "none";
       }
       else {
           div.style.display = "block";
       }
+    });
 
-
-  });
+    document.getElementById("blog-button").addEventListener("click", function ()  {
+     var div = document.getElementById("blog-hide");
+      if (div.style.display == "block") {
+          div.style.display = "none";
+      }
+      else {
+          div.style.display = "block";
+      }
+    });
 
 
   });
